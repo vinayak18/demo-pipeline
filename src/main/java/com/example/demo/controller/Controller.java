7500703bffe63b3ea626c29dcc16e046f03729b8
vinayak18 <vinayaksaraf1999@gmail.com>
2025-04-20 15:14:21 +0530

    Create Controller.java

---
+++ src/main/java/com/example/demo/controller/Controller.java
@@ -0,0 +1,14 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+	@GetMapping("/helloWorld")
+	public void message() {
+		System.out.println("Hello World");
+	}
+	
+}
